0b0668026912f1780dbb2e63d394b21824f61e45
Aditya Shinde <shinde.aditya386@gmail.com>
2017-12-22 21:24:16 -0500

    Added more tests to check iterator termination conditions.

--- tests/test_streamer.py
+++ tests/test_streamer.py
@@ -13,7 +13,7 @@ class TestStreamer(unittest.TestCase):
     def setUp(self):
         self.path="/home/adityas/Projects/Experiment_Results"
         self.files=get_data_files(self.path,["ztomatrix.txt"])
-        
+
     def test_single_line_getter(self):
         streamer=Streamer(self.files)
         lines=[]
@@ -21,3 +21,17 @@ class TestStreamer(unittest.TestCase):
             lines.append(streamer.get_single_line())
         self.assertEqual(len(lines),5)
 
+
+    def test_file_change(self):
+        streamer=Streamer(self.files)
+        lines=[]
+        for i in range(10000):
+            lines.append(streamer.get_single_line())
+        lines=list(filter(lambda x:x!=None,lines))
+        self.assertEqual(len(lines),10000)
+
+    def test_iterator_end(self):
+        streamer=Streamer(self.files)
+        for i in range(1000000):
+            line=streamer.get_single_line()
+        self.assertIsNone(line)
